bc61c22c6c2f5736e94421a08ba834b6d3dd8464
chenziyu <ziyucrc@gmail.com>
2021-09-29 15:06:02 +0800

    interactive widget done (#2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'fooderlich_theme.dart';
 import 'home.dart';
+import 'models/models.dart';
 
 void main() {
   runApp(const Fooderlich());
@@ -11,6 +13,15 @@ class Fooderlich extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = FooderlichTheme.light();
-    return MaterialApp(theme: theme, title: 'Fooderlich', home: const Home());
+    return MaterialApp(
+        theme: theme,
+        title: 'Fooderlich',
+        home: MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (context) => TabManager()),
+            ChangeNotifierProvider(create: (context)=> GroceryManager(),)
+          ],
+          child: const Home(),
+        ));
   }
 }
