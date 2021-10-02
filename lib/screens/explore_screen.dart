a39ee134ffe87b6bd5fd9d96ddefebfb0b5404e6
chenziyu <ziyucrc@gmail.com>
2021-10-02 09:47:46 +0800

    route and navigattion (#3)

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,58 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:fooderlich/models/explore_data.dart';
+
 import '../api/mock_fooderlich_service.dart';
 import '../components/components.dart';
+import '../models/models.dart';
 
-class ExploreScreen extends StatefulWidget {
-  ExploreScreen({Key key}) : super(key: key);
-
-  @override
-  State<ExploreScreen> createState() => _ExploreScreenState();
-}
-
-class _ExploreScreenState extends State<ExploreScreen> {
+class ExploreScreen extends StatelessWidget {
   final mockService = MockFooderlichService();
-  ScrollController _controller;
-  @override
-  void initState() {
-    _controller = ScrollController();
-    _controller.addListener(_scrollListener);
-    super.initState();
-  }
 
-  void _scrollListener() {
-    if (_controller.offset >= _controller.position.maxScrollExtent &&
-        !_controller.position.outOfRange) {
-      print('i am at the bottom!');
-    }
-    if (_controller.offset <= _controller.position.minScrollExtent &&
-        !_controller.position.outOfRange) {
-      print('i am at the top!');
-    }
-  }
+  ExploreScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<ExploreData>(
-        future: mockService.getExploreData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            final recipes = snapshot.data.todayRecipes;
-            final posts = snapshot.data.friendPosts;
-            return ListView(
-              controller: _controller,
-              scrollDirection: Axis.vertical,
-              children: [
-                TodayRecipeListView(recipes: recipes),
-                const SizedBox(height: 16),
-                FriendPostListView(friendPosts: posts),
-              ],
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        });
+    return FutureBuilder(
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              const SizedBox(height: 16),
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? [])
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,38 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:fooderlich/models/grocery_manager.dart';
 import 'package:provider/provider.dart';
-import 'empty_grocery_screen.dart';
-import 'grocery_item_screen.dart';
-import 'grocery_list_screen.dart';
+
+import '../models/models.dart';
+import 'screens.dart';
+
 class GroceryScreen extends StatelessWidget {
-  const GroceryScreen({Key key}):super(key: key);
+  const GroceryScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          final manager = Provider.of<GroceryManager>(context, listen: false);
-          Navigator.push(context, MaterialPageRoute(builder: (context){
-            return GroceryItemScreen(
-              onCreate: (item){
-                manager.addItem(item);
-                Navigator.pop(context);
-              },
-            );
-          }));
-        },
         child: const Icon(Icons.add),
+        onPressed: () {
+          Provider.of<GroceryManager>(context, listen: false).createNewItem();
+        },
       ),
-      body: buildGroceryScreen(context),
+      body: buildGroceryScreen(),
     );
   }
-  Widget buildGroceryScreen(BuildContext context) {
-    return Consumer<GroceryManager>(builder: (context, manager, child){
-      if(manager.groceryItems.isNotEmpty) {
-        return GroceryListScreen(manager: manager,);
-      } else {
-        return  const EmptyGroceryScreen();
-      }
-    });
+
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(
+      builder: (context, manager, child) {
+        if (manager.groceryItems.isNotEmpty) {
+          return GroceryListScreen(manager: manager);
+        } else {
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
   }
 }
