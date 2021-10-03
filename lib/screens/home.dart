cb46061b4efa529944036967ffba976bc2aa9920
chenziyu <ziyucrc@gmail.com>
2021-10-03 14:28:24 +0800

    feat: deep link done (#4)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../models/models.dart';
 import 'explore_screen.dart';
 import 'grocery_screen.dart';
 import 'recipes_screen.dart';
 
 class Home extends StatefulWidget {
-  static MaterialPage page(int currentTab){
+  static MaterialPage page(int currentTab) {
     return MaterialPage(
       name: FooderlichPages.home,
       key: ValueKey(FooderlichPages.home),
-      child: Home(currentTab: currentTab,),
+      child: Home(
+        currentTab: currentTab,
+      ),
     );
   }
 
@@ -34,42 +37,52 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppStateManager>(builder: (context, appStateManager, child){
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Fooderlich',
-            style: Theme.of(context).textTheme.headline6,
+    return Consumer<AppStateManager>(
+      builder: (
+        context,
+        appStateManager,
+        child,
+      ) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Fooderlich',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            actions: [
+              profileButton(),
+            ],
           ),
-          actions: [
-            profileButton(),
-          ],
-        ),
-        body: IndexedStack(index: widget.currentTab, children: pages),
-        bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
-          currentIndex: widget.currentTab,
-          onTap: (index) {
-            Provider.of<AppStateManager>(context, listen: false).goToTab(index);
-          },
-          items: <BottomNavigationBarItem>[
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.explore),
-              label: 'Explore',
-            ),
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.book),
-              label: 'Recipes',
-            ),
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.list),
-              label: 'To Buy',
-            ),
-          ],
-        ),
-      );
-    },);
-    // TODO: Add closing },);
+          body: IndexedStack(
+            index: widget.currentTab,
+            children: pages,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor:
+                Theme.of(context).textSelectionTheme.selectionColor,
+            currentIndex: widget.currentTab,
+            onTap: (index) {
+              Provider.of<AppStateManager>(context, listen: false)
+                  .goToTab(index);
+            },
+            items: <BottomNavigationBarItem>[
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.explore),
+                label: 'Explore',
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'Recipes',
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                label: 'To Buy',
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   Widget profileButton() {
@@ -78,12 +91,11 @@ class _HomeState extends State<Home> {
       child: GestureDetector(
         child: const CircleAvatar(
           backgroundColor: Colors.transparent,
-          backgroundImage: AssetImage(
-            'assets/profile_pics/person_stef.jpeg',
-          ),
+          backgroundImage: AssetImage('assets/profile_pics/person_stef.jpeg'),
         ),
         onTap: () {
-          Provider.of<ProfileManager>(context, listen: false).tapOnProfile(true);
+          Provider.of<ProfileManager>(context, listen: false)
+              .tapOnProfile(true);
         },
       ),
     );
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../models/models.dart';
 
 class LoginScreen extends StatelessWidget {
   static MaterialPage page() {
     return MaterialPage(
-        name: FooderlichPages.loginPath,
-        key: ValueKey(FooderlichPages.loginPath),
-        child: const LoginScreen());
+      name: FooderlichPages.loginPath,
+      key: ValueKey(FooderlichPages.loginPath),
+      child: const LoginScreen(),
+    );
   }
 
   final String? username;
@@ -26,30 +28,24 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 148),
-                const SizedBox(
-                  height: 180,
-                  child: Image(
-                    image: AssetImage(
-                      'assets/fooderlich_assets/rw_logo.png',
-                    ),
-                  ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 200,
+                child: Image(
+                  image: AssetImage('assets/fooderlich_assets/rw_logo.png'),
                 ),
-                const SizedBox(height: 16),
-                buildTextfield(username ?? '🍔 username'),
-                const SizedBox(height: 16),
-                buildTextfield('🎹 password'),
-                const SizedBox(height: 16),
-                buildButton(context),
-              ],
-            ),
+              ),
+              const SizedBox(height: 16),
+              buildTextfield(username ?? '🍔 username'),
+              const SizedBox(height: 16),
+              buildTextfield('🎹 password'),
+              const SizedBox(height: 16),
+              buildButton(context),
+            ],
           ),
         ),
       ),
@@ -61,17 +57,14 @@ class LoginScreen extends StatelessWidget {
       height: 55,
       child: MaterialButton(
         color: rwColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
         child: const Text(
           'Login',
           style: TextStyle(color: Colors.white),
         ),
         onPressed: () async {
-          Provider.of<AppStateManager>(context, listen: false).login(
-            'mockUsername','mockPassword'
-          );
+          Provider.of<AppStateManager>(context, listen: false)
+              .login('mockUsername', 'mockPassword');
         },
       ),
     );
@@ -80,7 +73,6 @@ class LoginScreen extends StatelessWidget {
   Widget buildTextfield(String hintText) {
     return TextField(
       cursorColor: rwColor,
-      cursorHeight: 18,
       decoration: InputDecoration(
         border: const OutlineInputBorder(
           borderSide: BorderSide(
@@ -89,9 +81,7 @@ class LoginScreen extends StatelessWidget {
           ),
         ),
         focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.green,
-          ),
+          borderSide: BorderSide(color: Colors.green),
         ),
         hintText: hintText,
         hintStyle: const TextStyle(height: 0.5),
